795eac15b735c2527e6b525659ce050576ae099b
Mim Mony <mimmony7799@gmail.com>
2026-09-02 09:48:37 +0600

    Modernize home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,8 +17,7 @@ public class HomeController : Controller
     }
     public IActionResult Index()
     {
-        
-        return RedirectToAction("Index", "Jobs");
+        return View();
     }
     public IActionResult Admin()
     {
